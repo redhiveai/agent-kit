551bc1a39c1ccf705465c721fcdfcdb9fb2c1238
silverbeeai <213038331+silverbeeai@users.noreply.github.com>
2025-05-30 10:16:55 +0530

    Refactor core exports and add actions module; update example configuration and mpc implementation

--- packages/core/src/types/agent.ts
+++ packages/core/src/types/agent.ts
@@ -1,61 +1,59 @@
-import { ethers } from "ethers";
-import type { Config } from "./config";
-import type { Action } from "./action";
-import type { Plugin } from "./plugin";
+import { ethers } from 'ethers'
+import type { Action } from './action'
+import type { Config } from './config'
+import type { Plugin } from './plugin'
 
 export type IAgent = {
-    privider: ethers.AbstractProvider
-    wallet: ethers.Wallet
-    config: Config
+  privider: ethers.AbstractProvider
+  wallet: ethers.Wallet
+  config: Config
 }
 
-type PluginMethods<T> = T extends Plugin ? T["methods"] : Record<string, never>;
+type PluginMethods<T> = T extends Plugin ? T['methods'] : Record<string, never>
 
 export class AgentKit<TPlugins = Record<string, never>> implements IAgent {
-    public privider: ethers.AbstractProvider
-    public config: Config
-    public wallet: ethers.Wallet
+  public privider: ethers.AbstractProvider
+  public config: Config
+  public wallet: ethers.Wallet
 
-    private plugins: Map<string, Plugin> = new Map();
-    public methods: TPlugins = {} as TPlugins;
-    public actions: Action[] = [];
+  private plugins: Map<string, Plugin> = new Map()
+  public methods: TPlugins = {} as TPlugins
+  public actions: Action[] = []
 
-    constructor(wallet: ethers.Wallet, config: Config) {
-        this.privider = ethers.getDefaultProvider(config.rpcURL)
-        this.wallet = wallet.connect(this.privider)
-        this.config = config
+  constructor(wallet: ethers.Wallet, config: Config) {
+    this.privider = ethers.getDefaultProvider(config.rpcURL)
+    this.wallet = wallet.connect(this.privider)
+    this.config = config
+  }
+
+  use<P extends Plugin>(plugin: P): AgentKit<TPlugins & PluginMethods<P>> {
+    if (this.plugins.has(plugin.name)) {
+      return this as AgentKit<TPlugins & PluginMethods<P>>
+    }
+    plugin.initialize(this as AgentKit)
+
+    for (const [methodName, method] of Object.entries(plugin.methods)) {
+      if ((this.methods as Record<string, unknown>)[methodName]) {
+        throw new Error(`Method ${methodName} already exists in methods`)
+      }
+      ;(this.methods as Record<string, unknown>)[methodName] =
+        method.bind(plugin)
     }
 
-    use<P extends Plugin>(
-        plugin: P,
-    ): AgentKit<TPlugins & PluginMethods<P>> {
-        if (this.plugins.has(plugin.name)) {
-            return this as AgentKit<TPlugins & PluginMethods<P>>;
-        }
-        plugin.initialize(this as AgentKit);
-
-        for (const [methodName, method] of Object.entries(plugin.methods)) {
-            if ((this.methods as Record<string, unknown>)[methodName]) {
-                throw new Error(`Method ${methodName} already exists in methods`);
-            }
-            (this.methods as Record<string, unknown>)[methodName] =
-                method.bind(plugin);
-        }
-
-        for (const action of plugin.actions) {
-            this.actions.push(action);
-        }
-
-        this.plugins.set(plugin.name, plugin);
-        return this as AgentKit<TPlugins & PluginMethods<P>>;
+    for (const action of plugin.actions) {
+      this.actions.push(action)
     }
+
+    this.plugins.set(plugin.name, plugin)
+    return this as AgentKit<TPlugins & PluginMethods<P>>
+  }
 }
 
-const kit = new AgentKit(
-    new ethers.Wallet("0x..."),
-    {
-        rpcURL: "https://rpc.ankr.com/eth",
-        chainId: 1,
-        quicknodeEndpoint: "https:/your-quicknode-endpoint",
-    }
-)
+// const kit = new AgentKit(
+//     new ethers.Wallet("0x..."),
+//     {
+//         rpcURL: "https://rpc.ankr.com/eth",
+//         chainId: 1,
+//         quicknodeEndpoint: "https:/your-quicknode-endpoint",
+//     }
+// )
--- packages/example/src/mpc.ts
+++ packages/example/src/mpc.ts
@@ -1,11 +1,54 @@
-import { AgentKit } from '@redhive/agent-kit-core';
-import { ethers } from "ethers";
+import {
+  AgentKit,
+  bridge,
+  lidoStake,
+  nativeToUSDPrice,
+  resolveENS,
+  resolveToken,
+  startMcpServer,
+  swapTokens,
+  transferERC20,
+  transferNative,
+  walletBalance,
+  walletBalances,
+  walletERC20Balance,
+  walletNFTs,
+  walletTransactions,
+  type Action,
+} from '@redhive/agent-kit-core'
+import { ethers } from 'ethers'
 
-const kit = new AgentKit(
-    new ethers.Wallet("0x..."),
-    {
-        rpcURL: "https://rpc.ankr.com/eth",
-        chainId: 1,
-        quicknodeEndpoint: "https:/your-quicknode-endpoint",
-    }
-)
+const kit = new AgentKit(new ethers.Wallet('0x....'), {
+  rpcURL: 'https://rpc.ankr.com/eth_sepolia',
+  chainId: 11155111,
+  quicknodeEndpoint: 'https://rpc.quicknode.com/eth_sepolia',
+})
+
+// Add your required actions here
+const mcp_actions: Record<string, Action> = {
+  bridge,
+  resolveENS,
+  lidoStake,
+  nativeToUSDPrice,
+  walletNFTs,
+  resolveToken,
+  swapTokens,
+  transferERC20,
+  transferNative,
+  walletBalance,
+  walletBalances,
+  walletERC20Balance,
+  walletTransactions,
+}
+
+for (const action of kit.actions) {
+  mcp_actions[action.name] = action
+}
+
+startMcpServer(mcp_actions, kit, { name: 'evm-agent', version: '0.0.1' })
+  .then((server) => {
+    console.log('Server is running on port 3000')
+  })
+  .catch((error) => {
+    console.error('Error starting MCP server:', error)
+  })
